281b8d0928033f46bb1a6d2e69fa32f21aa6983f
Scott Fennell <slfennell01@gmail.com>
2024-05-23 22:19:47 -0400

    Fix issues with code
    Fix accelerate within  car class, structure of EV extended class, and ending output statement.

--- cc_3.js
+++ cc_3.js
@@ -9,23 +9,24 @@ class car{
         this.make = make;
         this.speed = speed;
     }
-    accelerate(){
-        this.speed += 10;
-        console.log(this.makw, " accelerated. New speed: ", this.speed, " km/h");
+    accelerate(num){
+        this.speed += num;
+        console.log(this.make, " accelerated, new speed: ", this.speed, "km/h");
     }
     brake(){
         this.speed -= 5;
-        console.log(this.make, " braked. New speed: ", this.speed, " km/h");
+        console.log(this.make, " braked, new speed: ", this.speed, "km/h");
     }
 }
 class EV extends car{
-    constructor(charge){
+    constructor(make, speed, charge){
+        super(make, speed);
         this.charge = charge;
     }
     //2.) Implement a chargeBattery Method and chargeTo argument 
     chargeBattery(chargeTo){
         this.charge = chargeTo;
-        console.log(this.make, " battery charged to ",this.charge,"%");
+        console.log(this.make, " battery charged to ",this.charge, "%");
     }
     //3.) Override the accelerate method in EV class.Speed Increase 20 and battery change by 1.
     accelerate(){
@@ -37,5 +38,10 @@ class EV extends car{
 
 //4.) Create an EV object using the provided test data and call accelerate, brake, and chargeBattery.
 const car1 = new EV('Tesla', 120, 23);
-console.log(car1.make, " starting at: ", car1.speed, " km/h, charge is ", car1.chargeBattery, "%");
-console.log("Speed changing:\n", car1.accelerate(), "\n", car1.chargeBattery(), "\n", car1.accelerate(), "\n", car1.chargeBattery(), "\n", car1.brake());
+console.log(car1.make, " starting at: ", car1.speed, " km/h, charge is ", car1.charge, "%");
+console.log("Speed and charge changes:");
+car1.accelerate();
+car1.chargeBattery(60);
+car1.accelerate();
+car1.chargeBattery(70);
+car1.brake();
