d4b09884168a9b818e38466375437791be243385
Scott Fennell <slfennell01@gmail.com>
2024-05-23 21:42:29 -0400

    Override accelerate method in EV class to adjust speed and charge
    Override accelerate method in EV class to adjust speed and charge

--- cc_3.js
+++ cc_3.js
@@ -15,15 +15,22 @@ class car{
     }
     brake(){
         this.speed -= 5;
-        console.log(this.make, " braked. New speed: ", this.speed, "km/h");
+        console.log(this.make, " braked. New speed: ", this.speed, " km/h");
     }
 }
 class EV extends car{
     constructor(charge){
         this.charge = charge;
     }
+    //2.) Implement a chargeBattery Method and chargeTo argument 
     chargeBattery(chargeTo){
         this.charge = chargeTo;
         console.log(this.make, " battery charged to ",this.charge,"%");
+    }
+    //3.) Override the accelerate method in EV class.Speed Increase 20 and battery change by 1.
+    accelerate(){
+        super.accelerate(20);
+        this.charge -= 1;
+        console.log(this.make, " going at ", this.speed, " km/h, with a charge of ", this.charge, "%"); 
     }
 }
